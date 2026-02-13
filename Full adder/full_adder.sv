f7280129129cdf296dbbaada1d8ae476543e0798
Saiteja7-VLSI <saitejatudum123@gmail.com>
2026-02-13 21:35:11 +0530

    Create full_adder.sv

---
+++ Full adder/full_adder.sv
@@ -0,0 +1,10 @@
+module full_adder(
+  input logic [3:0]a,
+  input logic [3:0]b,
+  input logic cin,
+  output logic [3:0]sum,
+  output logic cout);
+  
+  assign {cout,sum} = a + b + cin;
+  
+endmodule
